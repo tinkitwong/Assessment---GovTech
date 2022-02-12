f9986d7d41e62b5a958aceabbe9afab33c6fb656
tinkitwong <tinkitwong@gmail.com>
2022-02-12 19:06:16 +0800

    Set up create household route

--- app.js
+++ app.js
@@ -2,19 +2,24 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 
+/** MW Config */
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 // DB setup
 const db = require('./db/models')
-db.sequelize.sync({ alter: true })
-.then((res) => {
+db.sequelize.sync({ force: true }).then((res) => {
     console.log('Checked current db state and made necessary changes to match defined models')
-})
-.catch((err) => {
+}).catch((err) => {
     throw(err)
 })
 
 // MVC pattern
 // app.use('/api/db', require('./db/routes/general.routes'))
 app.use('/api/household', require('./household/routes/household.routes'))
+
+// app.use('/', (req, res)=>{console.log('hello')})
 
 // Error Handling
 app.use((err, req, res, next) => {
